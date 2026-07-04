b6182a4dc3d63ec8a54b06257428dff8ef40bbba
NyxieLucy <nyxlucianis@gmail.com>
2026-07-04 12:33:14 +0100

    yeah first session

---
+++ 1-calc-for-syntax/notes.md
@@ -0,0 +1,9 @@
+for file exportation u use
+
+`g++` instead of `gcc` for C
+
+# variable declaration
+
+declare the type of the variable, folowed by the value
+
+to print it out use ``cout << the_variable << std::endl
---
+++ 1-calc-for-syntax/take1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+// same as C apparently
+int main() {
+  int number1 = 1;
+  std::cout << number1 << std::endl;
+  return 0;
+}
+// fellas it's not like C
+// i was trying to export it using ```gcc 1-calc-for-syntax/take1.cpp -o
+// take1``` in the terminal turns out you should use ```g++
+// 1-calc-for-syntax/take1.cpp -o take1```
