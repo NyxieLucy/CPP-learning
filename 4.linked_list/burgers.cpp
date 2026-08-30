b63706c9c97235044ea3121f1a7c79e46e6fc2b6
NyxieLucy <nyxlucianis@gmail.com>
2026-08-30 16:45:16 +0100

    hamburgers

--- 4.linked_list/burgers.cpp
+++ 4.linked_list/burgers.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <iostream>
 struct Node { // struct that holds different variable types for the same purpose
   int value;  // the value
   Node *next; // pointer to the next memory space
@@ -10,5 +12,7 @@ Node *createNode(int value) { // idk why it only takes the value when the struct
   newNode->next = nullptr; // that's the pointer
   return newNode;          // returning the new node... not the value (int)
 }
-
+void printlist(Node *head) {
+  std::for_each(Node * head){std::cout << "index: "}
+};
 int main() {}
